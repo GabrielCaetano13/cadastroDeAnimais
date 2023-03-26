343dbdcd0dbcef11491b91acbb920af3eb6b0393
GabrielCaetano13 <gabriel011depaulac@gmail.com>
2023-03-26 20:47:42 -0300

    calculate win/lose for distance day

--- js/ganhoEPerda.js
+++ js/ganhoEPerda.js
@@ -1,10 +1,16 @@
-let pesos = document.querySelectorAll('.peso-animal');
+let pesos = document.querySelectorAll('.peso-animal')
+let dataPesagem = document.querySelectorAll('.data-pesagem')
 
 for (let i = 1; i < pesos.length; i++) {
-    const pesoAtual = parseFloat(pesos[i].textContent);
-    const pesoAnterior = parseFloat(pesos[i - 1].textContent);
-    let diferenca = pesoAtual - pesoAnterior;
-    const ganhoPerda = document.createElement('span');
+    const dataAtual = new Date((dataPesagem[i].textContent))
+    const dataAnterior = new Date((dataPesagem[i -1].textContent))
+    let diferencaData =  Math.floor((dataAtual - dataAnterior) / (1000 * 60 * 60 * 24))
+
+    const pesoAtual = parseFloat(pesos[i].textContent)
+    const pesoAnterior = parseFloat(pesos[i - 1].textContent)
+    let diferenca = (pesoAtual - pesoAnterior) / diferencaData
+
+    const ganhoPerda = document.createElement('span')
     if (diferenca > 0) {
         diferenca = `+${diferenca.toFixed(3)} kg`
     } else {
