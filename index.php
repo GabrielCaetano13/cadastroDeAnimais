f4b4a6dfa56cbcf8a69f4216a216435d978a6eb9
GabrielCaetano13 <gabriel011depaulac@gmail.com>
2023-03-26 20:47:20 -0300

    alter bg-color, add buttons

--- index.php
+++ index.php
@@ -6,25 +6,24 @@
     <title>Pecuária Brasil</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
   </head>
-  <body>
+  <body data-bs-theme="dark" style="color: white;">
       
-    <nav class="navbar navbar-expand-lg bg-dark">
+    <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
-            <img src="https://www.pecuariabrasilassessoria.com.br/img/logo_3.png" width="150px" class="px-2" alt="">
-            <a class="navbar-brand px-1" href="#" style="color: white;">Cadastro</a>
-            <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand px-1" href="#" style="color: black;">Cadastro</a>
+            <button class="navbar-toggler bg-dark" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" style="color: white;" aria-current="page" href="./index.php">Home</a>
+                        <a class="nav-link active" style="color: black;" aria-current="page" href="./index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" style="color: white;" href="?page=cadastro">Cadastrar animal</a>
+                        <a class="nav-link" style="color: black;" href="?page=cadastro">Cadastrar animal</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" style="color: white;" href="?page=listar">Listar Animais</a>
+                        <a class="nav-link" style="color: black;" href="?page=listar">Listar Animais</a>
                     </li>
                 </ul>
             </div>
@@ -63,6 +62,23 @@
                             break;
                         default:
                             echo "<h1>Bem vindo(a) ao sistema</h1>";
+                            echo "
+                                <div class='row'>
+                                    <div class='col mt-3'>
+                                        <div class='content'>
+                                            <strong><p class='mt-3'>Cadastre um animal colocando suas informações, como seu número de identificação, sexo e data de nasicmeto.</p></strong>
+                                            <button class='btn btn-primary'><a class='nav-link' href='?page=cadastro'>Cadastre um animal</a></button>
+                                        </div>
+                                    </div>
+                                    <div class='col mt-3'>
+                                        <div class='content'>
+                                            <strong><p class='mt-3'>Verifique a lista de animais cadastrados, podendo adicionar informações sobre seu peso.</p></strong>
+                                            <button class='btn btn-primary'><a class='nav-link' href='?page=listar'>Listar animais</a></button>
+                                        </div>
+                                    </div>
+                                </div>
+                            ";
+
                     }
                 ?>
             </div>
